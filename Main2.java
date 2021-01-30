01c9d1ee54c8006671f38fa9c39b754d2f4fa3c4
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-01-30 13:04:12 +0300

    Main2.java removed

--- Main2.java
+++
@@ -1,8 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        long price = 2000_000_000;
-        long count = 500_000_000;
-        long total = price + count;
-        System.out.println(total);
-    }
-}
